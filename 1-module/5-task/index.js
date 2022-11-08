04c7f4cdc7a7bcf5eab504dac718187b2291a5ad
Pavel Kobozev <pashakobozev@gmail.com>
2022-11-08 18:14:35 +0500

    1 module / 5-task : done

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  // if (str.length > maxlength) {
+  //   return str.substr(0, maxlength - 1) + "…";
+  // }
+  // return str;
+  // сначала решил способом выше, потом попытался решить в одну строку: 
+  return (str.length > maxlength) ? str.substr(0, maxlength - 1) + "…" : str;
 }
