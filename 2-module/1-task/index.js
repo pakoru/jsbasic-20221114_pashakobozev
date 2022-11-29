b3c5db8d35797e61b9b13abba2b8be7cf0013aa9
Pavel Kobozev <pashakobozev@gmail.com>
2022-11-29 05:16:02 +0500

    another commit

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,16 +1,8 @@
 function sumSalary(salaries) {
-<<<<<<< HEAD
-  const sum = Object.values(salaries).filter(Number.isInteger).reduce((a, b) => a + b, 0);
-  return sum;
-  // let sum = 0;
-  // for (key in salaries) {
-  //   if (Number.isInteger(salaries[key])) {
-  //     sum += salaries[key];
-  //   }
-  // }
-  // return sum;
-=======
-  return Object.values(salaries).filter(Number.isInteger).reduce((a, b) => a + b, 0);
->>>>>>> 9c685f37133f577868b0d9ff896482c14915495e
+  return Object
+  .values(salaries)
+  .filter(Number.isInteger)
+  .reduce((a, b) => a + b, 0);
+
 }
 
