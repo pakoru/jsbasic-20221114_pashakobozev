88c6bd9fb16823e8706cfaea5f8a0a50d3faf9ef
Pavel Kobozev <pashakobozev@gmail.com>
2022-12-08 15:44:40 +0500

    5 module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
-  // ваш код...
+  const toggleTextButton = document.querySelector('.toggle-text-button');
+  const text = document.querySelector('#text');
+  toggleTextButton.addEventListener('click', () => {
+    if (text.hidden === false) {
+      text.hidden = true;
+      return;
+    }
+    if (text.hidden === true) {
+      text.hidden = false;
+      return;
+    }
+  }
+  )
 }
