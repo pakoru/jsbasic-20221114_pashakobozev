ffc1407608f61a54ee35e80b76dd3d918892d624
Pavel Kobozev <pashakobozev@gmail.com>
2022-12-12 18:09:02 +0500

    правки внесены

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -2,14 +2,7 @@ function toggleText() {
   const toggleTextButton = document.querySelector('.toggle-text-button');
   const text = document.querySelector('#text');
   toggleTextButton.addEventListener('click', () => {
-    if (text.hidden === false) {
-      text.hidden = true;
-      return;
-    }
-    if (text.hidden === true) {
-      text.hidden = false;
-      return;
-    }
+    text.hidden = !text.hidden;
   }
   )
 }
